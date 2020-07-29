21ecb07d1e0475c541acd5002203b10e3645d174
bulat kt <bulat.kt@yandex.ru>
2020-07-29 16:49:54 +0300

    adding app files

---
+++ routes/cards.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+const cardsPath = path.join(__dirname, '../data/cards.json');
+const cards = JSON.parse(fs.readFileSync(cardsPath, { encoding: 'utf8' }));
+
+
+router.get('/', (req,res) => {
+  res.send(cards);
+})
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+const usersPath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersPath, { encoding: 'utf8' }));
+
+
+router.get('/', (req,res) => {
+  res.send(users);
+});
+
+router.get('/:id',((req, res) => {
+  const user = users.find((item) => item._id === req.params.id);
+
+  if (!user) {
+    res.status(404).send({ "message": "Нет пользователя с таким id" });
+  } else {
+    res.send(user);
+  }
+}))
+
+
+module.exports = router;
