f96bd032bd26d9af66cf2bd968dbf4e995be1687
bulat kt <bulat.kt@yandex.ru>
2020-07-31 16:13:54 +0300

    error handling improved

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const usersPath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(usersPath, { encoding: 'utf8' }));
+// const users = JSON.parse(fs.readFileSync(usersPath, { encoding: 'utf8' }));
 
 router.get('/', (req, res) => {
   const reader = fs.createReadStream(usersPath, { encoding: 'utf8' });
@@ -17,12 +17,19 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', ((req, res) => {
-  const user = users.find((item) => item._id === req.params.id);
-  if (!user) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-  } else {
-    res.send(user);
-  }
+  const reader = fs.createReadStream(usersPath, { encoding: 'utf8' });
+  reader.on('error', () => {
+    res.status(500).send({ Error: 'Reading error' });
+  });
+  reader.on('open', () => {
+    const users = JSON.parse(fs.readFileSync(usersPath, { encoding: 'utf8' }));
+    const user = users.find((item) => item._id === req.params.id);
+    if (!user) {
+      res.status(404).send({ message: 'Нет пользователя с таким id' });
+    } else {
+      res.send(user);
+    }
+  });
 }));
 
 module.exports = router;
